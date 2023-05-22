f6a5a7a2c88aa8c175429c860c1a49a06d928a29
Shahiduzzaman Tauhid <shahiduzzamantauhid@gmail.com>
2023-05-22 21:49:12 +0600

    project initialized

---
+++ src/Components/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            this is home or main page
+        </div>
+    );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["acid"],
+  },
+}
+
